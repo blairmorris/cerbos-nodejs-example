0496f9116c828e170afaa5ca2e4b3df2aa60959c
Blair Morris <blair.morris@moodys.com>
2023-08-24 17:23:24 -0400

    Initial Commit - cloned from cerbos/demo-admin-api

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# syntax=docker/dockerfile:1
+FROM node:19 AS build
+WORKDIR /app
+COPY ["client/package.json", "client/package-lock.json*", "./"]
+RUN npm i
+COPY client/ .
+# https://github.com/parcel-bundler/parcel/issues/7126
+RUN rm -rf .parcel-cache/
+RUN npm run build
+
+FROM golang:1.19
+COPY --from=build /app/dist /app/client/dist
+WORKDIR /app
+COPY go.mod ./
+COPY go.sum ./
+RUN go mod download
+COPY main.go ./
+RUN go build -o /server
+CMD [ "/server" ]
---
+++ client/src/Editor.tsx
@@ -0,0 +1,117 @@
+// @ts-ignore
+import JSONWorker from 'url:monaco-editor/esm/vs/language/json/json.worker.js';
+// @ts-ignore
+import CSSWorker from 'url:monaco-editor/esm/vs/language/css/css.worker.js';
+// @ts-ignore
+import HTMLWorker from 'url:monaco-editor/esm/vs/language/html/html.worker.js';
+// @ts-ignore
+import TSWorker from 'url:monaco-editor/esm/vs/language/typescript/ts.worker.js';
+// @ts-ignore
+import EditorWorker from 'url:monaco-editor/esm/vs/editor/editor.worker.js';
+import React from 'react';
+import axios from 'axios';
+import * as monaco from 'monaco-editor';
+
+self.MonacoEnvironment = {
+    getWorkerUrl: function (_, label: string) {
+        if (label === 'json') {
+            return JSONWorker;
+        }
+        if (label === 'css' || label === 'scss' || label === 'less') {
+            return CSSWorker;
+        }
+        if (label === 'html' || label === 'handlebars' || label === 'razor') {
+            return HTMLWorker;
+        }
+        if (label === 'typescript' || label === 'javascript') {
+            return TSWorker;
+        }
+        return EditorWorker;
+    }
+};
+
+export default function Editor(props: {
+    policyId: string;
+    handleUpdatePolicyFn: any;
+}) {
+    const container = React.useRef(null);
+    const editor = React.useRef(null);
+    const [isValid, setIsValid] = React.useState(true);
+    const [errString, setErrString] = React.useState("");
+
+    const handleTextChange = () => {
+        // @ts-ignore
+        const text = editor.current.getModel().getValue();
+        axios
+            .patch("http://localhost:8090/validate", { policy: text })
+            .then(() => {
+                setIsValid(true);
+            })
+            .catch((e) => {
+                setErrString(e.response.data);
+                setIsValid(false);
+            });
+    };
+
+    React.useEffect(() => {
+        if (editor.current !== null) return;
+        // @ts-ignore
+        editor.current = monaco.editor.create(container.current, {
+            value: "",
+            language: "yaml",
+        });
+
+        // attach listener
+        // @ts-ignore
+        editor.current.getModel().onDidChangeContent(() => {
+            handleTextChange();
+        });
+    }, []);
+
+    React.useEffect(() => {
+        if (editor.current === null) return;
+        if (props.policyId === "") return;
+        axios
+            .get("http://localhost:8090/policy?id=" + props.policyId)
+            .then((resp) => {
+                // @ts-ignore
+                editor.current.getModel().setValue(resp.data.policies[0]);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }, [props.policyId]);
+
+    const handleUpdate = () => {
+        // @ts-ignore
+        const text = editor.current.getModel().getValue();
+        props.handleUpdatePolicyFn(text);
+    };
+
+    return (
+        <div>
+            <div
+                ref={container}
+                style={{ height: "400px", border: "1px solid grey" }}
+            ></div>
+            <div className="flex flex-row font-medium mt-5 border-solid border-2 border-grey rounded p-2">
+                <button
+                    disabled={!isValid}
+                    className={
+                        isValid
+                            ? "bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-1 mr-3 rounded"
+                            : "bg-gray-300 text-white font-bold px-2 py-1 mr-3 rounded"
+                    }
+                    onClick={handleUpdate}
+                >
+                    {isValid ? "Save" : "Invalid"}
+                </button>
+                {isValid ? (
+                    <p className="flex items-center">✅ Policy valid</p>
+                ) : (
+                    <p className="flex items-center">❌ Policy invalid: {errString}</p>
+                )}
+            </div>
+        </div>
+    );
+}
